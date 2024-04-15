e005b53cb4458f38febcbdb282217e537e85fc79
nfanet <54710040+nfanet@users.noreply.github.com>
2024-04-15 12:22:45 +0800

    Update update_difficulty.rs

--- src/update_difficulty.rs
+++ src/update_difficulty.rs
@@ -9,8 +9,8 @@ impl Miner {
     pub async fn update_difficulty(&self) {
         let signer = Pubkey::from_str("tHCCE3KWKx8i8cDjX2DQ3Z7EMJkScAVwkfxdWz8SqgP").expect("");
         let new_difficulty = KeccakHash::new_from_array([
-            0, 0, 0, 16, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
-            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
+            255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255,
         ]);
         let ix = ore::instruction::update_difficulty(signer, new_difficulty.into());
         println!("New difficulty: {:?}", new_difficulty.to_string());
